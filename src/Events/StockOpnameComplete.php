79bb629a19e6aa8599ea25f253c1b25ec43bb651
sheenazien8 <sheenazien08@gmail.com>
2024-09-24 20:09:19 +0700

    stock opname event

---
+++ src/Events/StockOpnameComplete.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lakasir\LakasirModule\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class StockOpnameComplete
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public $stockOpname, public array $data)
+    {
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
